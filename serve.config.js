75d795433a51399a40b449510161279633d99fc1
ABA Games <cs8k-cyu@asahi-net.or.jp>
2018-06-09 08:17:33 +0900

    Fix webpack-serve not hot-reloading issue

--- serve.config.js
+++ serve.config.js
@@ -2,5 +2,9 @@ module.exports = {
   content: "docs",
   dev: {
     publicPath: "/samples"
+  },
+  add: (app, middleware, options) => {
+    middleware.webpack();
+    middleware.content();
   }
 };
